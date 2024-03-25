66dbe91d0cc5985cdb43f9d392519eacfb19b1c1
Sachin Divakar <divakar2.sachin2@gmail.com>
2024-03-25 06:06:58 +0530

    Pass env variable to docker container

--- knexfile.js
+++ knexfile.js
@@ -3,10 +3,10 @@
 const sharedPostgresConfig = {
   client: "pg",
   connection: {
-    host: process.env.DB_HOST || 'localhost', // Use the correct default or ensure the environment variable is set
-    user: process.env.DB_USER || 'testuser', // Default to 'testuser' or ensure the environment variable is set
-    password: process.env.DB_PASS || 'testpassword', // Same as above
-    database: process.env.DB_NAME || 'testdb', // Same as above
+    host: process.env.DB_HOST || "localhost",
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
   },
   pool: { min: 2, max: 10 },
   migrations: {
